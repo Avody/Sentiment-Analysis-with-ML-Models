e527654aced936c1a8997d9dc10734af0acba334
Odysseas Avramopoulos <odavramop@gmail.com>
2022-02-23 17:34:18 +0200

    Update and rename Project.py to Bernoulli_LR_SVM_XGB.py

--- Bernoulli_LR_SVM_XGB.py
+++ Bernoulli_LR_SVM_XGB.py
@@ -1,3 +1,4 @@
+######### Bernoulli, SVM, Logistic Regression XGboost ##########
 # utilities
 import re
 import numpy as np
@@ -8,8 +9,8 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 # nltk
 import nltk
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import RegexpTokenizer
+from nltk.stem import PorterStemmer
+
 # sklearn
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import BernoulliNB
@@ -18,75 +19,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import confusion_matrix, classification_report
 
+import xgboost as xgb
+
+
 
 # Importing the dataset
 DATASET_COLUMNS=['target','ids','date','flag','user','text']
 DATASET_ENCODING = "ISO-8859-1"
 df = pd.read_csv (r'Dataset_2.csv' , encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
-print(df)
 
 
-df.head()
-
-
-df.columns
-
-#length of the dataset
-print('length of data is', len(df))
-
-#data information
-df.info()
-
-#datatypes of columns
-df.dtypes
-
-#Checking for Null values
-np.sum(df.isnull().any(axis=1))
-
-
-#Rows and columns in the dataset
-print('Count of columns in the data is:  ', len(df.columns))
-print('Count of rows in the data is:  ', len(df))
-
-
-#Check unique Target Values
-df['target'].unique()
-
-
-#Check the number of target values
-df['target'].nunique()
-
-
-
-# Plotting the distribution for dataset.
-ax = df.groupby('target').count().plot(kind='bar', title='Distribution of data',legend=False)
-ax.set_xticklabels(['Negative','Positive'], rotation=0)
-# Storing data in lists.
-text, sentiment = list(df['text']), list(df['target'])
-
-
-
-#plot the sentiment
-import seaborn as sns
-sns.countplot(x='target', data=df)
-
-
-#Data Preprocessing
-
+########### Data Preprocessing ############
 
 #select text and target for analysis
 data=df[['text','target']]
 
-
-
 #Replacing the values to ease understanding. (Assigning 1 to Positive sentiment 4)
 data['target'] = data['target'].replace(4,1)
-
-
-
-#Print unique values of target variables
-data['target'].unique()
-
 
 
 #Separating positive and negative tweets
@@ -94,22 +43,16 @@ data_pos = data[data['target'] == 1]
 data_neg = data[data['target'] == 0]
 
 
-
 #taking one fourth data so we can run on our machine easily
-data_pos = data_pos.iloc[:int(20000)]
-data_neg = data_neg.iloc[:int(20000)]
-
-
-
+data_pos = data_pos.iloc[:int(200000)]
+data_neg = data_neg.iloc[:int(200000)]
 
 #Combining positive and negative tweets
 dataset = pd.concat([data_pos, data_neg])
 
 
-
 #Making statement text in lower case
 dataset['text']=dataset['text'].str.lower()
-dataset['text'].tail()
 
 
 #Defining set containing all stopwords in English.
@@ -129,16 +72,11 @@ stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an
              'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
              "youve", 'your', 'yours', 'yourself', 'yourselves']
 
-
-
 #Cleaning and removing the above stop words list from the tweet text
 STOPWORDS = set(stopwordlist)
 def cleaning_stopwords(text):
     return " ".join([word for word in str(text).split() if word not in STOPWORDS])
 dataset['text'] = dataset['text'].apply(lambda text: cleaning_stopwords(text))
-dataset['text'].head()
-
-
 
 
 #Cleaning and removing punctuations
@@ -149,7 +87,6 @@ def cleaning_punctuations(text):
     translator = str.maketrans('', '', punctuations_list)
     return text.translate(translator)
 dataset['text']= dataset['text'].apply(lambda x: cleaning_punctuations(x))
-dataset['text'].tail()
 
 
 
@@ -157,8 +94,6 @@ dataset['text'].tail()
 def cleaning_repeating_char(text):
     return re.sub(r'(.)1+', r'1', text)
 dataset['text'] = dataset['text'].apply(lambda x: cleaning_repeating_char(x))
-dataset['text'].tail()
-
 
 
 
@@ -166,67 +101,40 @@ dataset['text'].tail()
 def cleaning_URLs(data):
     return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
 dataset['text'] = dataset['text'].apply(lambda x: cleaning_URLs(x))
-dataset['text'].tail()
-
-
 
 
 #Cleaning and removing numbers
 def cleaning_numbers(data):
     return re.sub('[0-9]+', '', data)
 dataset['text'] = dataset['text'].apply(lambda x: cleaning_numbers(x))
-dataset['text'].tail()
+st=PorterStemmer()
 
+#Stemming tweets
+def stemming_sentence(sentence):   
+    words = nltk.word_tokenize(sentence)
+    res_words = []
+    for word in words:
+        res_words.append(st.stem(word))
+    return " ".join(res_words)
 
-
-
+dataset['text'] = dataset['text'].apply(lambda x: stemming_sentence(x)) 
 
 #Separating input feature and label
 X=dataset.text
 y=dataset.target
 
 
-
-
-#Plot a cloud of words for negative tweets
-data_neg = data['text'][:800000]
-plt.figure(figsize = (20,20))
-wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
-               collocations=False).generate(" ".join(data_neg))
-plt.imshow(wc)
-
-
-
-#Plot a cloud of words for positive tweets
-data_pos = data['text'][800000:]
-wc = WordCloud(max_words = 1000 , width = 1600 , height = 800,
-              collocations=False).generate(" ".join(data_pos))
-plt.figure(figsize = (20,20))
-plt.imshow(wc)
-
-
-
-
 # Separating the 95% data for training data and 5% for testing data
-X_train, X_test, y_train, y_test = train_test_split(dataset.text,dataset.target,test_size = 0.05, random_state =0)
-
-
-
-
-
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.05, random_state =0)
 
 #TF-IDF Vectorizer
 vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
 vectoriser.fit(X_train)
-print('No. of feature_words: ', len(vectoriser.get_feature_names()))
-
+print(X_train)
 
 #Transform the data using TF-IDF Vectorizer
 X_train = vectoriser.transform(X_train)
 X_test  = vectoriser.transform(X_test)
-
-
-
 
 #Model Bernoulli Naive Bayes
 BNBmodel = BernoulliNB()
@@ -234,44 +142,102 @@ BNBmodel.fit(X_train, y_train)
 y_pred1 = BNBmodel.predict(X_test)
 
 
-
-print(y_pred1)
-
-
 print(classification_report(y_test, y_pred1))
-
-
-
 #Model SVM(Support Vector Machine)
 SVCmodel = LinearSVC()
 SVCmodel.fit(X_train, y_train)
 y_pred2 = SVCmodel.predict(X_test)
-print(y_pred2)
 
 
 print(classification_report(y_test, y_pred2))
-
 
 # Model Logistic Regression
 LRmodel = LogisticRegression(C = 2, max_iter = 1000, n_jobs=-1)
 LRmodel.fit(X_train, y_train)
 y_pred3 = LRmodel.predict(X_test)
-print(y_pred3)
 
 
 print(classification_report(y_test, y_pred3))
 
 #Model XGBoost
-import xgboost as xgb
-xg = xgb.XGBClassifier()
+xg = xgb.XGBClassifier(use_label_encoder=False, eval_metric='error')
 xg.fit(X_train, y_train)
 y_pred4 = xg.predict(X_test)
 
-
-print(y_pred4)
-
-
 print(classification_report(y_test, y_pred4))
 
+########## Classify real tweets fetching them with twitter API ###########
 
+#Setting twitter API 
+import tweepy
+
+consumer_key = "z4eXbyALxvkOODCNafLiywMmr"
+consumer_secret_key = "Z1DmX40VkH6HsXfOlW2yObUQeBeoxmW1wIlS9MVwkUwpa3N5dS"
+
+access_token = '1372256857050517509-USUehtp3cqFAFUEjLdEm9nuQ4No8SX'
+access_token_secret = 'o0cKXbjgaF49adIZATEqUNnWYvGwsVW1OCpyi59LsPBxk'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+#Fetching tweets ordered by a topic
+
+keyword = input('Enter a field: ')
+count = 500
+#TWEETS
+tweets = tweepy.Cursor(api.search_tweets,q=keyword,count = 100,tweet_mode = 'extended',lang='en').items(count)
+#tweets = tweepy.Cursor(api.user_timeline,screen_name='JoeBiden',count = 100,tweet_mode = 'extended').items(count)
+
+
+#DATAFRAME OF TWEETS
+columns_t = ['User','Tweet']
+data_t = []
+for tweet in tweets:
+    data_t.append([tweet.user.screen_name, tweet.full_text])
+df_tweets = pd.DataFrame(data_t,columns=columns_t)
+
+
+#Clean tweets the same way with our data
+df_tweets['Tweet'] = df_tweets['Tweet'].str.lower()    
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: cleaning_stopwords(x))
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: cleaning_punctuations(x))
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: cleaning_repeating_char(x))
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: cleaning_URLs(x))
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: cleaning_numbers(x))
+df_tweets['Tweet'] = df_tweets['Tweet'].apply(lambda x: stemming_sentence(x))
+
+
+#Sentiment analysis with the already trained model 
+#Tweet to vector
+X_tweet = df_tweets.Tweet
+tweet_to_vector = vectoriser.transform(X_tweet)
+
+#Bernoulli
+tweet_predicted_BNB = BNBmodel.predict(tweet_to_vector)
+
+#Plot the results
+tweet_predicted_BNB = pd.DataFrame(tweet_predicted_BNB,columns=['category'])
+plotted_1 = tweet_predicted_BNB.value_counts().plot(kind='pie', x='Sentiment', autopct='%1.1f%%',figsize=(7,7))
+
+#SVC
+tweet_predicted_SVC = SVCmodel.predict(tweet_to_vector)
+
+#Plot the results
+tweet_predicted_SVC = pd.DataFrame(tweet_predicted_SVC,columns=['category'])
+plotted_2 = tweet_predicted_SVC.value_counts().plot(kind='pie',y='Sentiment', autopct='%1.1f%%',figsize=(7,7))
+
+#Logistic Regression
+tweet_predicted_LR = LRmodel.predict(tweet_to_vector)
+
+tweet_predicted_LR = pd.DataFrame(tweet_predicted_LR,columns=['category'])
+plotted_3 = tweet_predicted_LR.value_counts().plot(kind='pie',y='Sentiment', autopct='%1.1f%%',figsize=(7,7))
+
+#XGBoost
+tweet_predicted_XGB = xg.predict(tweet_to_vector)
+
+#Plot the results
+tweet_predicted_XGB = pd.DataFrame(tweet_predicted_XGB,columns=['category'])
+plotted_4 = tweet_predicted_XGB.value_counts().plot(kind='pie',y='Sentiment', autopct='%1.1f%%',figsize=(7,7))
 
